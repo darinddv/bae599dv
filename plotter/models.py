7b76bc530a690cde45b918953285f63fc70e310a
darinddv <47702590+darinddv@users.noreply.github.com>
2022-11-02 21:42:38 -0400

    add locations table

--- plotter/models.py
+++ plotter/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class LocationLoads(models.Model):
+    location_name = models.CharField(max_length=50)
+    batteries = models.FloatField()
+    loads = models.JSONField()
+
+    def __str__(self):
+        return f'{self.location_name} ({self.loads})'
